21b9e9b18b62e3e32e54f2e09c9fdef7114a3a80
mcrlgarcia <gar18055@byui.edu>
2021-01-06 07:01:43 -0600

    change

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,4 +1,4 @@
 /*Reference:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/formatToParts */
-let options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, };
+let options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
