517fa3fb0a8c081906aaabd1303c723123af214b
mcrlgarcia <gar18055@byui.edu>
2021-01-06 12:39:16 -0600

    change

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,5 +1,7 @@
 /*Reference:
 https://www.w3schools.com/jsref/prop_doc_lastmodified.asp
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
 */
 
 var d = document.lastModified;
