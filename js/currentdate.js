a5912dac5ad39fe599ef55748c2ef6ff912508d9
mcrlgarcia <gar18055@byui.edu>
2021-01-06 18:45:30 -0600

    change

--- js/currentdate.js
+++ js/currentdate.js
@@ -4,5 +4,6 @@ https://www.w3schools.com/jsref/prop_doc_lastmodified.asp
 https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
 */
 
-var d = document.lastModified;
-document.getElementById("currentdate").innerHTML = d;
+var string = document.lastModified;
+document.getElementById("currentdate").innerHTML = string;
+
