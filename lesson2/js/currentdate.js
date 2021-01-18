2b10d6c69f79e9ec52742a2652c7006329ecbf9b
mcrlgarcia <gar18055@byui.edu>
2021-01-18 09:38:18 -0600

    change

--- lesson2/js/currentdate.js
+++ lesson2/js/currentdate.js
@@ -1,18 +1,21 @@
-let { month, date, hours, minutes, seconds } = newFunction();
+// let { month, date, hours, minutes, seconds } = newFunction();
 
 
-document.getElementById('currentdate').innerHTML = `&nbsp &nbsp${month}/ ${date}/ ${year}&nbsp &nbsp${hours}:${minutes}:${seconds}`; 
+// document.getElementById('currentdate').innerHTML = `&nbsp &nbsp${month}/ ${date}/ ${year}&nbsp &nbsp${hours}:${minutes}:${seconds}`; 
 
 
-function newFunction() {
-    let today = new Date();
+// function newFunction() {
+//     let today = new Date();
 
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-    let date = today.getDate();
+//     let month = today.getMonth() + 1;
+//     let year = today.getFullYear();
+//     let date = today.getDate();
 
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    return { month, date, hours, minutes, seconds };
-}
+//     let hours = today.getHours();
+//     let minutes = today.getMinutes();
+//     let seconds = today.getSeconds();
+//     return { month, date, hours, minutes, seconds };
+// }
+
+var x = document.lastModified;
+document.getElementById("currentdate").innerHTML = x;
